460d0507617cd2881bf45eb9bfb60c682f41855e
Dayaldonadkar <Dayalpython@gmail.com>
2023-10-14 19:18:37 +0530

    create page created

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div className="flex justify-center">
       <div className="w-[70%]">
@@ -8,12 +11,24 @@ const Create = () => {
         <form className="space-y-8 pt-5">
           <div className="space-y-1">
             <label htmlFor="">Name</label>
-            <input className="w-[100%] border border-black" type="text" />
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              className="w-[100%] border border-black"
+              type="text"
+            />
           </div>
           <div className="space-y-1">
             <label htmlFor="">Email</label>
-            <input className="w-[100%] border border-black" type="text" />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              className="w-[100%] border border-black"
+              type="text"
+            />
           </div>
+
+          <button className="py-1 px-2 bg-blue-500 text-black">Submit</button>
         </form>
       </div>
     </div>
