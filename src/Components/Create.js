f0d93f059b5167f1cb744325d0983b6c16e64b90
Dayaldonadkar <Dayalpython@gmail.com>
2023-10-15 01:55:47 +0530

    with erroes

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Create = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
+  const header = { "Access-control-Allow-origin": "*" };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    axios.post("https://mockapi.io/projects/652a9ba54791d884f1fd21e6", {
+      name: name,
+      email: email,
+      header,
+    });
+  };
+
   return (
     <div className="flex justify-center">
       <div className="w-[70%]">
         <h1 className="text-3xl pt-5">Create</h1>
-        <form className="space-y-8 pt-5">
+        <form onSubmit={(e) => e.preventDefault} className="space-y-8 pt-5">
           <div className="space-y-1">
             <label htmlFor="">Name</label>
             <input
@@ -28,7 +41,12 @@ const Create = () => {
             />
           </div>
 
-          <button className="py-1 px-2 bg-blue-500 text-black">Submit</button>
+          <button
+            className="py-1 px-2 bg-blue-500 text-black"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
